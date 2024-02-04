f2cc7abd3c812d837d7fbd92f137d53b3eab3a9a
Chumki khatun <chumkikhatun370@gmail.com>
2024-02-04 10:23:27 +0600

    complete subscribe section

--- src/Components/HomeComponents/Subscribe.jsx
+++ src/Components/HomeComponents/Subscribe.jsx
@@ -1,9 +1,27 @@
-
+import { useEffect } from 'react';
+import subscribe from '../../assets/subscribe.jpg'
+import Aos from 'aos';
 
 const Subscribe = () => {
+    useEffect(() =>{
+        Aos.init()
+      },[])
+    const subscribeImage ={
+        backgroundImage: `url(${subscribe})`,  // Corrected typo here
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%'
+    }
     return (
-        <div>
-            
+        <div className='mb-20 bg-gray-100 dark:bg-gray-800 text-white' data-aos="zoom-in" style={subscribeImage}>
+            <div className='container backdrop-blur-sm py-10'>
+                <div className='space-y-6 max-w-xl mx-auto'>
+                    <h1 className='text-2xl !text-center sm:text-left sm:text-4xl font-semibold'>Get Notified About New Products</h1>
+                    <input data-aos="fade-up" type="email" name="" id="" placeholder='Enter your email' className='w-full p-3'/>
+                </div>
+            </div>
         </div>
     );
 };
