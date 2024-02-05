1b637d494298c9e2693daaa9fb370ca687a757ab
Chumki khatun <chumkikhatun370@gmail.com>
2024-02-05 12:31:19 +0600

    added logo section about page

---
+++ src/Components/About/LogoSection.jsx
@@ -0,0 +1,17 @@
+
+
+const LogoSection = () => {
+    return (
+        <div className="container">
+           <div className="flex justify-evenly text-5xl font-semibold my-10 text-gray-500">
+           <h1 className="hover:text-primary/80 hover:transition-all hover:scale-105 duration-300">Nike</h1>
+            <h1 className="hover:text-primary/80 hover:transition-all hover:scale-105 duration-300">Adidas</h1>
+            <h1 className="hover:text-primary/80 hover:transition-all hover:scale-105 duration-300">Vans</h1>
+            <h1 className="hover:text-primary/80 hover:transition-all hover:scale-105 duration-300">Puma</h1>
+           </div>
+            
+        </div>
+    );
+};
+
+export default LogoSection;
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,12 @@
 import FirstSection from "../../Components/About/FirstSection";
+import LogoSection from "../../Components/About/LogoSection";
 
 
 const About = () => {
     return (
         <div className="mb-10">
             <FirstSection/>
+            <LogoSection/>
            
         </div>
     );
